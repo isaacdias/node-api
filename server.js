f713ee8421536d2670de786dfa6328137bbc694c
Isaac Dias <isaacardias@gmail.com>
2020-06-13 10:39:56 -0300

    criando estrutura

---
+++ server.js
@@ -0,0 +1,9 @@
+// importar o express 
+const express = require('express');
+
+// executar o express
+const app = express();
+
+// a aplicação irá ouvir a porta 3001 do navegador
+app.listen(3001);
+
