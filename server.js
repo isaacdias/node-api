7ce34dd5fb6d599c9122acf5207603404d07bd20
Isaac Dias <isaacardias@gmail.com>
2020-06-13 11:09:23 -0300

    criando a primeira rota com o express

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const express = require('express');
 // executar o express
 const app = express();
 
+// criando a primeira rota
+//  ('/') significa que é a rota raiz da aplicação
+// o segundo parametro desse get recebee uma funçao com dois parametros (req, res)
+// req simboliza a requisição que estou fazendo ao servidor
+// res tem a ver com a resposta que darei para requisição
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+
 // a aplicação irá ouvir a porta 3001 do navegador
 app.listen(3001);
 
